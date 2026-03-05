bfc63a2e6406296fadf15c4c4a8d7ff094efcaee
Federico Verdi <federicoverdi@me.com>
2026-03-05 22:38:57 +0100

    Nuova build --build

--- forge.config.js
+++ forge.config.js
@@ -70,7 +70,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
